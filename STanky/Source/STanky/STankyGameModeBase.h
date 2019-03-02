9345735f99e5bca857eeecb8021eda73da07bd00
Darkin <darkinb255@gmail.com>
2019-03-02 14:06:25 +0200

    Level Design 0

---
+++ STanky/Source/STanky/STankyGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "STankyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STANKY_API ASTankyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
